187f6b2e37e9f6ba0ce3ce2f77b60fcf35c0be9b
CrashedBboy <crashedbboy@gmail.com>
2014-10-31 23:10:19 +0800

    1.Adding extra plugin in to "extra" folder 2.Adding basic form into login and register page

---
+++ login.php
@@ -0,0 +1,13 @@
+<!--Hello developer,this page is for you to build the login form.-->
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="" method="post">
+			Account: <input type="text" name="sample1"><br/>
+			Password: <input type="text" name="sample2"><br/>
+			<input type="submit" value="Submit"><br/>
+		</form>
+		<a href="register.php">Not a member? Register Now!</a>
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,12 @@
+<!--Hello developer,this page is for you to build the register form.-->
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="" method="post">
+			Some Info: <input type="text" name="sample1"><br/>
+			Some Info: <input type="text" name="sample2"><br/>
+			<input type="submit" value="Submit">
+		</form>
+	</body>
+</html>
