74486dc820af42160353b5eddedca34d67215fe9
a0988358096 <a0988358096@gmail.com>
2014-11-08 23:15:17 +0800

    modify connectodata.php

--- connectodata.php
+++ connectodata.php
@@ -3,16 +3,16 @@
   //需要引入此檔案
   //資料庫主機設定
   $db_host = "localhost";
-  $db_table = "datasave";
-  $db_username = "root";
-  $db_password = "";
+  $db_name = "datasave";
+  $db_username = "chatspace";
+  $db_password = "snmg3";
   
   //設定連線
   if(!@mysql_connect($db_host, $db_username, $db_password))
   	die("資料連結失敗囉!");
   
   //連結資料庫
-  if(!@mysql_connect_db($db_table)) 
+  if(!@mysql_connect_db($db_name)) 
   	die("資料選擇失敗呀!");
   
   //設定字元校對
