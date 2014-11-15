8eca1d446c773b34df4dcb9546503c7f9a7429b6
a0988358096 <a0988358096@gmail.com>
2014-11-15 23:04:20 +0800

    modify and background

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <html>
 <head>
-	<meta http-equiv="Content-Type" conntent="text/html;charset=utf-8'" /> 
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
 	<!--屬性3種,內容-->
 	<title>MemberAdmin System - Homepage</title>
+        <link rel="stylesheet" type="text/css" href="indexback.css">
 </head>
 <body>
-	<form name="formal" method="post" action="login.php">  <!--傳遞方式,目標頁面-->
+	<form name="form1" method="post" action="login.php">  <!--傳遞方式,目標頁面-->
 		
-		<table width="600" border="1" valign="middle" align="center">
+		<table id='form' width="600" border="1" valign="middle" align="center">
 			<tr valign="middle"><td align="center">
 				<p>Hello Guys</p>
                 <label class="placeholder-label empty"><br>
