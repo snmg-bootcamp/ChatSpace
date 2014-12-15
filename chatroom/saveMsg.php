ce26c49929000d85da0f54ee65e733a6bb6599be
CrashedBboy <crashedbboy@gmail.com>
2014-12-16 03:18:13 +0800

    Adding message bubble

--- chatroom/saveMsg.php
+++ chatroom/saveMsg.php
@@ -1,9 +1,10 @@
 <?php
 	require_once("../include/connectodata.php");
 
-	if (isset($_POST["content"]) && isset($_POST["timestamp"])){
+	if (isset($_POST["content"]) && isset($_POST["timestamp"]) && isset($_POST["author"])){
 		$contentSecured = htmlspecialchars($_POST["content"]);
-		$sql = "INSERT INTO messages (content,time) VALUES ('$contentSecured','$_POST[timestamp]')";
+		$authorSecured = htmlspecialchars($_POST["author"]);
+		$sql = "INSERT INTO messages (author,content,time) VALUES ('$authorSecured','$contentSecured','$_POST[timestamp]')";
 		mysql_query( $sql);
 	}
 ?>
