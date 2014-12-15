27d3fad078b6e47ca3a2f25e4385acc1503c103d
CrashedBboy <crashedbboy@gmail.com>
2014-12-16 04:01:32 +0800

    fix bugs

--- chatroom.php
+++ chatroom.php
@@ -36,7 +36,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Setting<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-            								<li><a href="memberadmin">Member Admin</a></li>
+            								<li><a href="memberadmin.php">Member Admin</a></li>
             								<li class="divider"></li>
             								<li><a href="include/logout.php">Log out</a></li>
           							</ul>
