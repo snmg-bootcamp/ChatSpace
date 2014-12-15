b055f44b1c50b0df42fb950c4888340e33d621b2
CrashedBboy <crashedbboy@gmail.com>
2014-12-15 11:41:01 +0800

    Adding link to direct to memberAdmin,logout

--- chatroom.php
+++ chatroom.php
@@ -31,13 +31,11 @@
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="#">Link</a></li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Setting<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-            								<li><a href="#">Action</a></li>
-            								<li><a href="#">Another action</a></li>
-            								<li><a href="#">Something else here</a></li>
+            								<li><a href="memberadmin">Member Admin</a></li>
             								<li class="divider"></li>
-            								<li><a href="#">Separated link</a></li>
+            								<li><a href="include/logout.php">Log out</a></li>
           							</ul>
         						</li>
       					</ul>
