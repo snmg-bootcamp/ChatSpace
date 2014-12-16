68dfb7dc362d12a7b455a451e234277da344c017
CrashedBboy <crashedbboy@gmail.com>
2014-12-16 14:52:51 +0800

    Delete space in the file

--- chatroom/polling.php
+++ chatroom/polling.php
@@ -2,28 +2,20 @@
 	require_once("../include/connectodata.php");
 	set_time_limit(0);
 	$last_call = $_POST["timestamp"];
-
 	$sql = "SELECT * FROM messages WHERE time > '".$last_call."' ORDER BY time";
-
-
 	while (true) {
-		
 		$result = mysql_query($sql);
 		$num = mysql_num_rows($result);
-
 		if ($num > 0){
 			while($row = mysql_fetch_assoc($result)){
      			$json[] = $row;
 			}
-			//$messages = mysql_fetch_array($result);
 			$messages = json_encode($json);
-			
 			echo $messages;
 			break;
 		}else{
 			sleep( 1 );
 			continue;
 		}
-
 	}
 ?>
